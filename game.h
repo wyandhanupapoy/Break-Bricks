4f335223edbb6217c3d1db47d589ed99c95a6b15
wyandhanupapoy <wyandhanupapoy@gmail.com>
2025-03-02 14:59:16 +0700

    penghapusan file yang tidak perlu

--- game.h
+++ game.h
@@ -1,22 +1,127 @@
-//game.h
+// game.h - File berisi fungsi-fungsi utama game
 #ifndef GAME_H
 #define GAME_H
 
-// Fungsi deklarasi
-void setup();
-void drraw();
-void input();
-void logic();
-void gameOver();
-void restartGame();
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <conio.h>
+#include <windows.h>
 
-// Bola dan Paddle
-int ballX, ballY;
-int ballDirX, ballDirY;
-int paddleX, paddleWidth;
-int score;
+#include "definisi.h"
+#include "tampilan.h"
+#include "paddle.h"
+#include "bola.h"
+#include "blok.h"
 
-// Block
-int block[5][5];
+// Mulai level baru
+void mulaiLevelBaru(DataPermainan *data, Paddle *paddle, Bola *bola, Blok blok[JUMLAH_BLOK_BARIS][JUMLAH_BLOK_KOLOM]) {
+    data->level++;
+    data->kecepatan = data->kecepatan > 10 ? data->kecepatan - 10 : 10;
+    data->blok_tersisa = TOTAL_BLOK;
+    
+    // Reset posisi paddle dan bola
+    inisialisasiPaddle(paddle);
+    inisialisasiBola(bola, *paddle);
+    
+    // Setup blok baru
+    inisialisasiBlok(blok);
+}
 
-#endif
+// Proses input pemain
+void prosesInput(char input, Paddle *paddle, Bola *bola, int *bolaBergerak, int *gameSelesai) {
+    switch (input) {
+        case 'a': case 'A':
+            gerakkanPaddleKiri(paddle);
+            break;
+        case 'd': case 'D':
+            gerakkanPaddleKanan(paddle);
+            break;
+        case ' ': // Spasi untuk mulai bola bergerak
+            if (!(*bolaBergerak)) {
+                mulaiBola(bola);
+                *bolaBergerak = 1;
+            }
+            break;
+        case 'q': case 'Q':
+            *gameSelesai = 1;
+            break;
+    }
+}
+
+// Fungsi utama game
+void mainkanGame() {
+    char layar[BARIS][LEBAR_LAYAR];
+    DataPermainan data = {0, 1, 100, "Player", 0, 3, TOTAL_BLOK};
+    Paddle paddle;
+    Bola bola;
+    Blok blok[JUMLAH_BLOK_BARIS][JUMLAH_BLOK_KOLOM];
+    time_t waktuMulai, waktuSekarang;
+    char input;
+    int gameSelesai = 0;
+    int bolaBergerak = 0;
+    
+    // Sembunyikan kursor
+    sembunyikanKursor();
+    
+    // Setup awal permainan
+    printf("Masukkan nama Anda: ");
+    scanf("%s", data.nama);
+    
+    inisialisasiLayar(layar);
+    inisialisasiPaddle(&paddle);
+    inisialisasiBola(&bola, paddle);
+    inisialisasiBlok(blok);
+    
+    waktuMulai = time(NULL);
+    
+    // Loop utama game
+    while (!gameSelesai) {
+        // Perbarui waktu
+        waktuSekarang = time(NULL);
+        data.waktu_bermain = (int)(waktuSekarang - waktuMulai);
+        
+        // Kosongkan layar untuk diperbarui
+        inisialisasiLayar(layar);
+        
+        // Tampilkan informasi di sisi kiri
+        tampilkanInfoSisiKiri(layar, data);
+        
+        // Tampilkan objek game di sisi kanan
+        tampilkanPaddle(layar, paddle);
+        tampilkanBola(layar, bola);
+        tampilkanBlok(layar, blok);
+        
+        // Tampilkan layar
+        tampilkanLayar(layar, data);
+        
+        // Cek input pengguna
+        if (kbhit()) {
+            input = getch();
+            prosesInput(input, &paddle, &bola, &bolaBergerak, &gameSelesai);
+        }
+        
+        // Gerakan bola dan deteksi tabrakan
+        int hasilGerakan = gerakkanBola(&bola, paddle, blok, &data);
+        
+        // Cek hasil gerakan bola
+        if (hasilGerakan == 1) {
+            // Game over
+            gameSelesai = 1;
+        } else if (hasilGerakan == 2) {
+            // Level selesai
+            tampilkanLevelSelesai(data.level);
+            mulaiLevelBaru(&data, &paddle, &bola, blok);
+            bolaBergerak = 0;
+        }
+        
+        // Delay
+        Sleep(data.kecepatan);
+    }
+    
+    // Tampilkan pesan akhir permainan
+    tampilkanGameOver(data);
+}
+
+#endif // GAME_H
