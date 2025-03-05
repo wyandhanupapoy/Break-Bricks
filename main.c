6a07137673aaa2235b16366e14b478c52b90077d
wyandhanupapoy <wyandhanupapoy@gmail.com>
2025-03-05 08:35:44 +0700

    menambahkan main sebagai template pembuatan windows dari sdl

---
+++ main.c
@@ -0,0 +1,60 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_main.h>
+#include <stdio.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+int main(int argc, char* argv[]) {
+    // Inisialisasi SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL Initialization Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Membuat jendela
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window", 
+                                        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                        WINDOW_WIDTH, WINDOW_HEIGHT, 
+                                        SDL_WINDOW_SHOWN);
+    if (!window) {
+        printf("Window Creation Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Membuat renderer
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf("Renderer Creation Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Loop utama
+    int running = 1;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        // Mengatur warna latar belakang
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Menampilkan hasil render
+        SDL_RenderPresent(renderer);
+    }
+
+    // Membersihkan sumber daya
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
