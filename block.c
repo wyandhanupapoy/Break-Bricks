9753eaa764948392fe9993af07672fa68d29473c
nezyazulfa <zulfanezya@gmail.com>
2025-03-10 10:59:19 +0700

    Update block.c

---
+++ block.c
@@ -0,0 +1,33 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_main.h>
+#include "blok.h"
+
+Block blocks[ROWS][COLUMNS]; // Definisi array blok
+
+void initBlocks() {
+    int startX = 50, startY = 50;
+    int blockWidth = 70, blockHeight = 20;
+    int spacing = 5;
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLUMNS; j++) {
+            blocks[i][j].x = startX + j * (blockWidth + spacing);
+            blocks[i][j].y = startY + i * (blockHeight + spacing);
+            blocks[i][j].width = blockWidth;
+            blocks[i][j].height = blockHeight;
+            blocks[i][j].isDestroyed = 0;
+        }
+    }
+}
+
+void drawBlocks(SDL_Renderer *renderer) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLUMNS; j++) {
+            if (!blocks[i][j].isDestroyed) {
+                SDL_Rect rect = {blocks[i][j].x, blocks[i][j].y, blocks[i][j].width, blocks[i][j].height};
+                SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);  // Warna biru
+                SDL_RenderFillRect(renderer, &rect);
+            }
+        }
+    }
+}
