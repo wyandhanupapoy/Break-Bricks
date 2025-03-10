8e498add4d1d77cda1455d8ce2441ff8d6c88d4a
nezyazulfa <zulfanezya@gmail.com>
2025-03-10 10:59:36 +0700

    Update block.h

---
+++ block.h
@@ -0,0 +1,22 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include <SDL.h> // Gunakan SDL.h tanpa SDL2/, sesuaikan dengan path instalasi
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define ROWS 5
+#define COLUMNS 10
+
+typedef struct {
+    int x, y;
+    int width, height;
+    int isDestroyed;
+} Block;
+
+extern Block blocks[ROWS][COLUMNS];  // Deklarasi array blok
+
+void initBlocks();
+void drawBlocks(SDL_Renderer *renderer);
+
+#endif
