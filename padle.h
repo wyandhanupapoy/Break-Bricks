6c36c435b50540a2ee8fb05fa19ba6c464e1d3ab
hanzz78 <raihanabubakar5@gmail.com>
2025-03-12 11:57:48 +0700

    konsep array

--- padle.h
+++ padle.h
@@ -1,10 +1,15 @@
 #ifndef PADLE_H
 #define PADLE_H
 
-#include <raylib.h>
+#include "raylib.h"
 
+// Ukuran layar
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+
+// Konstanta untuk paddle
+#define PADDLE_ROWS 1    // Jumlah baris paddle (bisa diubah jika ingin lebih dari 1)
+#define PADDLE_COLS 2    // Jumlah paddle dalam satu baris
 #define PADDLE_WIDTH 100
 #define PADDLE_HEIGHT 20
 #define PADDLE_SPEED 5
@@ -16,8 +21,8 @@ typedef struct {
 } Paddle;
 
 // Deklarasi fungsi
-void InitPaddle(Paddle *paddle);
-void UpdatePaddle(Paddle *paddle);
-void DrawPaddle(Paddle *paddle);
+void InitPaddles(Paddle paddles[PADDLE_ROWS][PADDLE_COLS]);
+void UpdatePaddles(Paddle paddles[PADDLE_ROWS][PADDLE_COLS]);
+void DrawPaddles(Paddle paddles[PADDLE_ROWS][PADDLE_COLS]);
 
 #endif
