a588b5964ebd7a6b670cb46e0356e0408259e7bc
wyandhanupapoy <wyandhanupapoy@gmail.com>
2025-03-12 09:48:11 +0700

    pembuatan main.c untuk template dan penyatuan code

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "raylib.h"
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+int main(void) {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raylib Window");
+    SetTargetFPS(60);
+    
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+    
+    CloseWindow();
+    return 0;
+}
