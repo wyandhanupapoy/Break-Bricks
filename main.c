c6f6aa1354970683ab2cefe86520d2dbedafd8aa
MuhammadBrataH <muhammadbrata06@gmail.com>
2025-03-13 11:30:31 +0700

    main skor

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "block.h"
 #include "BOLA.h"
 #include "skor.h"
+#include "stopwatch.h"
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
@@ -12,6 +13,8 @@ int main()
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout - Paddle");
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Break Bricks - Skor");
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout - Blocks");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Stopwatch in Raylib");
+
     SetTargetFPS(60);
 
     Paddle paddles[PADDLE_ROWS][PADDLE_COLS]; // Deklarasi array paddle
@@ -26,23 +29,29 @@ int main()
     Skor skor[MAX_PLAYERS];
     InitSkor(skor);
 
+    Stopwatch sw[ROWS][COLS]; // Deklarasi stopwatch sebagai array 2D
+    InitStopwatch(sw);        // Inisialisasi stopwatch
+
     while (!WindowShouldClose())
     {
         // Update logika paddle
         UpdatePaddles(paddles);
         UpdateBola(bola);
+        UpdateStopwatch(sw); // Update stopwatch setiap frame
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
         ClearBackground(BLACK);
 
         DrawText("Breakout Game!", 300, 20, 20, BLACK);
+        DrawText("Stopwatch Example", 280, 20, 20, BLACK);
 
         // Gambar semua paddle
         DrawPaddles(paddles);
         DrawBlocks(blocks);
         DrawBola(bola);
         DrawSkor(skor);
+        DrawStopwatch(sw); // Gambar stopwatch di tengah layar
 
         EndDrawing();
     }
