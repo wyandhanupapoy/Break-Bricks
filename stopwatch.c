3a44f465a20b375b6bdf72319e8d91253d35e0a4
hanzz78 <raihanabubakar5@gmail.com>
2025-03-13 11:05:46 +0700

    stopwatch

---
+++ stopwatch.c
@@ -0,0 +1,35 @@
+#include "stopwatch.h"
+#include <stdio.h>
+
+// Inisialisasi stopwatch
+void InitStopwatch(Stopwatch sw[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            sw[i][j].time = 0.0f;
+            sw[i][j].running = true; // Stopwatch mulai berjalan
+        }
+    }
+}
+
+// Update waktu stopwatch jika berjalan
+void UpdateStopwatch(Stopwatch sw[ROWS][COLS]) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            if (sw[i][j].running) {
+                sw[i][j].time += GetFrameTime(); // Tambahkan waktu setiap frame
+            }
+        }
+    }
+}
+
+// Menampilkan stopwatch di layar
+void DrawStopwatch(Stopwatch sw[ROWS][COLS]) {
+    char timeText[20];
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            sprintf(timeText, "Time: %.2f", sw[i][j].time);
+            DrawText(timeText, 350, 280, 30, BLACK); // Posisi tengah layar
+        }
+    }
+}
