ef5ad59a00ce48a3c3eb632061ab0ff013429d62
MuhammadBrataH <muhammadbrata06@gmail.com>
2025-03-13 11:26:56 +0700

    main skor

--- main.c
+++ main.c
@@ -2,11 +2,16 @@
 #include "padle.h"
 #include "block.h"
 #include "BOLA.h"
+#include "skor.h"
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
 int main()
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout - Paddle");
-    InitWindow(800, 600, "Breakout - Blocks");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Break Bricks - Skor");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout - Blocks");
     SetTargetFPS(60);
 
     Paddle paddles[PADDLE_ROWS][PADDLE_COLS]; // Deklarasi array paddle
@@ -18,6 +23,9 @@ int main()
     Bola bola[ROWS][COLS];
     InitBola(bola);
 
+    Skor skor[MAX_PLAYERS];
+    InitSkor(skor);
+
     while (!WindowShouldClose())
     {
         // Update logika paddle
@@ -26,6 +34,7 @@ int main()
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
         DrawText("Breakout Game!", 300, 20, 20, BLACK);
 
@@ -33,6 +42,7 @@ int main()
         DrawPaddles(paddles);
         DrawBlocks(blocks);
         DrawBola(bola);
+        DrawSkor(skor);
 
         EndDrawing();
     }
