07d7b33c0d59f656b92e00a3b0cf82f6f7ce9f41
wyandhanupapoy <wyandhanupapoy@gmail.com>
2025-03-16 22:32:52 +0700

    pembuatan leaderboard

---
+++ leaderboard.h
@@ -0,0 +1,18 @@
+#ifndef LEADERBOARD_H
+#define LEADERBOARD_H
+
+#include <raylib.h>
+
+#define MAX_PLAYERS 10 // Maximum number of players in the leaderboard
+
+typedef struct {
+    char name[20]; // Player's name
+    int score;     // Player's score
+    float time;    // Time taken to finish the game
+} LeaderboardEntry;
+
+void InitLeaderboard(LeaderboardEntry leaderboard[MAX_PLAYERS]);
+void AddToLeaderboard(LeaderboardEntry leaderboard[MAX_PLAYERS], const char* name, int score, float time);
+void DrawLeaderboard(LeaderboardEntry leaderboard[MAX_PLAYERS]);
+
+#endif // LEADERBOARD_H
