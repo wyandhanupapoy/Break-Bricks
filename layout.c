6c727329605bc6b19613e32bc3d1d22464b00eb7
wyandhanupapoy <wyandhanupapoy@gmail.com>
2025-03-17 09:35:09 +0700

    pengambilan dari branch layout

--- layout.c
+++ layout.c
@@ -1,15 +1,20 @@
 #include "layout.h"
 #include "raylib.h"
 
-#define PANEL_WIDTH 230
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
+static int screenWidth = DEFAULT_SCREEN_WIDTH; // Variabel untuk menyimpan lebar layar
+static int screenHeight = DEFAULT_SCREEN_HEIGHT; // Variabel untuk menyimpan tinggi layar
+
+void SetScreenSize(int width, int height) {
+    screenWidth = width;
+    screenHeight = height;
+    SetWindowSize(screenWidth, screenHeight); // Mengatur ukuran jendela
+}
 
 void DrawGameLayout(Block blocks[][BLOCK_COLS], Paddle paddles[][PADDLE_COLS], Bola bola[][BOLA_COLS], Stopwatch sw[][STOPWATCH_COLS], Nyawa nyawa[][NYAWA_KOLOM]) {
     ClearBackground(RAYWHITE);
     
     // Panel kanan (warna abu-abu muda)
-    DrawRectangle(SCREEN_WIDTH - PANEL_WIDTH, 0, PANEL_WIDTH, SCREEN_HEIGHT, LIGHTGRAY);
+    DrawRectangle(screenWidth - PANEL_WIDTH, 0, PANEL_WIDTH, screenHeight, LIGHTGRAY);
     
     // Gambar blok, paddle, dan bola di area utama
     DrawBlocks(blocks);
@@ -17,7 +22,7 @@ void DrawGameLayout(Block blocks[][BLOCK_COLS], Paddle paddles[][PADDLE_COLS], B
     DrawBola(bola);
     
     // Posisi awal untuk UI di panel kanan
-    int uiX = SCREEN_WIDTH - PANEL_WIDTH + 20;  // Geser sedikit dari tepi
+    int uiX = screenWidth - PANEL_WIDTH + 20;  // Geser sedikit dari tepi
     int nyawaY = 20;  // Nyawa di bagian atas
     int stopwatchY = nyawaY + 40;  // Stopwatch di bawah nyawa dengan jarak 40px
 
---
+++ layout.h
@@ -0,0 +1,18 @@
+#ifndef LAYOUT_H
+#define LAYOUT_H
+
+#include <raylib.h>
+#include "block.h"
+#include "paddle.h"
+#include "BOLA.h"
+#include "nyawa.h"
+#include "stopwatch.h"
+
+// Tambahkan definisi untuk ukuran layar
+#define DEFAULT_SCREEN_WIDTH 1000
+#define DEFAULT_SCREEN_HEIGHT 650
+
+void DrawGameLayout(Block blocks[][BLOCK_COLS], Paddle paddles[][PADDLE_COLS], Bola bola[][BOLA_COLS], Stopwatch sw[][STOPWATCH_COLS], Nyawa nyawa[][NYAWA_KOLOM]);
+void SetScreenSize(int width, int height); // Fungsi untuk mengatur ukuran layar
+
+#endif // LAYOUT_H
