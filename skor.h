9798e99cb0074ec71aecd9bc13ab016400cd1833
nsitizen <sitisoviyyah@gmail.com>
2025-03-23 21:48:46 +0700

    mengubah kode skor agar bisa terintegrasi dengan stopwatch dan menghapus file yang tidak diperlukan

--- skor.h
+++ skor.h
@@ -1,17 +1,26 @@
+//Nama Pembuat: Siti Soviyyah
+//Nama Fitur: Header skor.h
+//Deskripsi: File ini berguna untuk memanggil fungsi-fungsi yang ada pada file skor.c
+
+
 #ifndef SKOR_H
 #define SKOR_H
 
-#include "raylib.h"
+#include <raylib.h>
+#include "leaderboard.h"
+#include "game_state.h"
 
-#define MAX_PLAYERS 1 // Jumlah pemain maksimal (bisa disesuaikan)
+#define MAX_SCORE_PER_BLOCK 50
+#define MIN_SCORE_PER_BLOCK 5
+#define TIME_FACTOR 10.0f
 
-typedef struct {
+typedef struct
+{
     int score; // Skor pemain
 } Skor;
 
 void InitSkor(Skor skor[MAX_PLAYERS]);
-void TambahSkor(Skor *skor, int nilai);
-void DrawSkor(Skor skor[MAX_PLAYERS]);
-void ResetSkor(Skor *skor);
+void TambahSkorDenganWaktu(Skor *skor, float elapsedTime);
+void DrawSkor(Skor skor[MAX_PLAYERS], int x, int y);
 
 #endif // SKOR_H
