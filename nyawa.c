62c9533c101883f07a6a0f1a28a85ddd3e51f2c2
MuhammadBrataH <muhammadbrata06@gmail.com>
2025-03-23 21:33:30 +0700

    fixed code nyawa

--- nyawa.c
+++ nyawa.c
@@ -1,43 +1,82 @@
 #include "nyawa.h"
+#include "layout.h"
+#include "raylib.h"
 
-void InitNyawa(Nyawa nyawa[NYAWA_BARIS][NYAWA_KOLOM], int totalNyawa) {
-    for (int i = 0; i < NYAWA_KOLOM; i++) {
-        nyawa[0][i].rect.x = 10 + (NYAWA_WIDTH + NYAWA_SPACING) * i;
-        nyawa[0][i].rect.y = 10;
-        nyawa[0][i].rect.width = NYAWA_WIDTH;
-        nyawa[0][i].rect.height = NYAWA_HEIGHT;
-        nyawa[0][i].aktif = i < totalNyawa;
+static float nyawaPosX = 0;                                   // Posisi default X
+static float nyawaPosY = 0;                                   // Posisi default Y
+static float nyawaScale = (float)DEFAULT_NYAWA_SIZE / 100.0f; // Skala nyawa
+static Texture2D nyawaTexture;
+
+// 🔹 Memuat gambar nyawa
+void LoadNyawaTexture()
+{
+    nyawaTexture = LoadTexture("assets/images/heart.png");
+}
+
+// 🔹 Membersihkan gambar nyawa dari memori
+void UnloadNyawaTexture()
+{
+    UnloadTexture(nyawaTexture);
+}
+
+// 🔹 Mengatur posisi nyawa
+void SetNyawaPosition(float x, float y)
+{
+    nyawaPosX = x;
+    nyawaPosY = y;
+}
+
+// 🔹 Mengatur ukuran nyawa
+void SetNyawaSize(float size)
+{
+    nyawaScale = size / 100.0f;
+}
+
+// 🔹 Inisialisasi jumlah nyawa
+void InitNyawa(Nyawa nyawa[NYAWA_BARIS][NYAWA_KOLOM], int totalNyawa)
+{
+    for (int i = 0; i < NYAWA_KOLOM; i++)
+    {
+        nyawa[0][i].aktif = (i < totalNyawa);
     }
 }
 
-void DrawNyawa(Nyawa nyawa[NYAWA_BARIS][NYAWA_KOLOM]) {
-    for (int i = 0; i < NYAWA_KOLOM; i++) {
-        if (nyawa[0][i].aktif) {
-            DrawRectangleRec(nyawa[0][i].rect, RED);
-        }
-    }
-}
-
-
-void KurangiNyawa(Nyawa nyawa[NYAWA_BARIS][NYAWA_KOLOM]) {
-    for (int i = 0; i < NYAWA_KOLOM; i++) {
-        if (nyawa[0][i].aktif) {
-            nyawa[0][i].aktif = false; // Mengurangi nyawa
+// 🔹 Kurangi nyawa jika kehilangan bola
+void KurangiNyawa(Nyawa nyawa[NYAWA_BARIS][NYAWA_KOLOM])
+{
+    for (int i = NYAWA_KOLOM - 1; i >= 0; i--)
+    {
+        if (nyawa[0][i].aktif)
+        {
+            nyawa[0][i].aktif = false;
             break;
         }
     }
 }
 
-bool AnyLivesLeft(Nyawa nyawa[NYAWA_BARIS][NYAWA_KOLOM]) {
-    for (int i = 0; i < NYAWA_KOLOM; i++) {
-        if (nyawa[0][i].aktif) return true;
+// 🔹 Cek apakah masih ada nyawa tersisa
+bool AnyLivesLeft(Nyawa nyawa[NYAWA_BARIS][NYAWA_KOLOM])
+{
+    for (int i = 0; i < NYAWA_KOLOM; i++)
+    {
+        if (nyawa[0][i].aktif)
+        {
+            return true;
+        }
     }
     return false;
 }
 
-void SetNyawaPosition(Nyawa nyawa[NYAWA_BARIS][NYAWA_KOLOM], float x, float y) {
-    for (int i = 0; i < NYAWA_KOLOM; i++) {
-        nyawa[0][i].rect.x = x + (NYAWA_WIDTH + NYAWA_SPACING) * i;
-        nyawa[0][i].rect.y = y;
+// 🔹 Gambar nyawa dengan ukuran dinamis & berjejer dari kanan ke kiri
+void DrawNyawa(Nyawa nyawa[NYAWA_BARIS][NYAWA_KOLOM])
+{
+    for (int i = 0; i < NYAWA_KOLOM; i++)
+    {
+        if (nyawa[0][i].aktif)
+        {
+            float posX = nyawaPosX - (i * ((nyawaTexture.width * nyawaScale) + NYAWA_SPACING));
+            float posY = nyawaPosY;
+            DrawTextureEx(nyawaTexture, (Vector2){posX, posY}, 0.0f, nyawaScale, WHITE);
+        }
     }
 }
