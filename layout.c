1013b6d58e517dad3c82d07b4f574e89fd41786b
nezyazulfa <nezya.zulfa.tif24@polban.ac.id>
2025-03-23 22:52:40 +0700

    penambahan layout

--- layout.c
+++ layout.c
@@ -3,38 +3,44 @@
 //Deskripsi: Fitur layout mengatur tampilan elemen-elemen dalam game **Break Bricks** agar lebih rapi dan intuitif. Arena permainan menampilkan paddle, bola, dan blok, sementara panel di sisi kanan menampilkan skor, nyawa, dan stopwatch. Instruksi kontrol juga ditampilkan untuk memudahkan pemain. Dengan tata letak yang terorganisir, permainan menjadi lebih nyaman dan interaktif.
 
 #include "layout.h"
+#include "raylib.h"
 #include <stdio.h>
 
-#define SCREEN_WIDTH 1000
-#define SCREEN_HEIGHT 650
+void DrawPauseScreen()
+{
+    DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, Fade(BLACK, 0.5f));
+    DrawText("PAUSED", 400, 300, 40, WHITE);
+    DrawText("Press P to resume", 390, 350, 20, WHITE);
+}
 
-void DrawGameLayout(Block blocks[BLOCK_ROWS][BLOCK_COLS], Paddle paddles[PADDLE_ROWS][PADDLE_COLS], Bola bola[BOLA_ROWS][BOLA_COLS], Stopwatch stopwatch[STOPWATCH_ROWS][STOPWATCH_COLS], Nyawa nyawa[NYAWA_BARIS][NYAWA_KOLOM], Skor skor[MAX_PLAYERS]) {
-    // Menggambar background
-    ClearBackground(BLACK); // Menggunakan warna latar belakang yang sesuai
+void DrawInfo()
+{
+    DrawText("Ahmad Habib Mutaqqin (241511065)", SCREEN_WIDTH / 2 - 160, 250, 20, WHITE);
+    DrawText("Muhammad Brata Hadinata (241511082)", SCREEN_WIDTH / 2 - 160, 300, 20, WHITE);
+    DrawText("Muhammad Raihan Abubakar (241511084)", SCREEN_WIDTH / 2 - 160, 350, 20, WHITE);
+    DrawText("Nezya Zulfa Fauziah (241511085)", SCREEN_WIDTH / 2 - 160, 400, 20, WHITE);
+    DrawText("Siti Soviyyah (241511090)", SCREEN_WIDTH / 2 - 160, 450, 20, WHITE);
+    DrawText("Wyandhanu Maulidan Nugraha** (241511092)", SCREEN_WIDTH / 2 - 160, 500, 20, WHITE);
+}
 
-    // Menggambar garis pembatas
-    DrawLine(835, 0, 835, SCREEN_HEIGHT, WHITE);
+void DrawGameOverScreen()
+{
+    ClearBackground(RAYWHITE);
+    DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+    DrawText("GAME OVER", 370, 300, 40, RED);
+    DrawText("Returning to menu...", 400, 350, 20, DARKGRAY);
+}
 
-    // Menggambar TataCaraPembatas
-    DrawRectangle(0, 600, 835, 50, WHITE);
-    DrawText("<- -> Bergerak       P - Pause       Esc - Exit", 150, 610, 20, BLACK);
+void DrawWinScreen()
+{
+    ClearBackground(RAYWHITE);
+    DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+    DrawText("YOU WIN!", 370, 300, 40, GREEN);
+    DrawText("Returning to menu...", 370, 350, 20, DARKGRAY);
+}
 
-    // Menggambar blok
-    DrawBlocks(blocks);
-
-    // Menggambar paddle
-    DrawPaddles(paddles);
-
-    // Menggambar bola
-    DrawBola(bola);
-
-    // Menggambar nyawa
-    SetNyawaPosition(nyawa, NYAWA_X, NYAWA_Y);
-    DrawNyawa(nyawa);
-
-    // Menggambar skor
-    DrawSkor(skor, SCORE_X, SCORE_Y); // Modifikasi DrawSkor untuk menerima posisi
-
-    // Menggambar stopwatch
-    DrawStopwatch(stopwatch, STOPWATCH_X, STOPWATCH_Y); // Modifikasi DrawStopwatch untuk menerima posisi
+void DrawControlInfo()
+{
+    DrawRectangle(0, 620, 835, 30, LIGHTGRAY);
+    DrawText("<- -> Move   |   P - Pause   |   F - Fullscreen   |   Esc - Exit   |   M - Mute", 15, 625, 20, BLACK);
 }
