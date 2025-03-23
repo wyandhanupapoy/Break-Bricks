dc63f2089c6a53a06887b12117fb084e5366d21b
nsitizen <sitisoviyyah@gmail.com>
2025-03-23 21:40:42 +0700

    memperbaiki kode untuk Sound Effect agar dapat berjalan di dalam game

---
+++ sound.h
@@ -0,0 +1,36 @@
+//Nama Pembuat: Siti Soviyyah
+//Nama Fitur: Header sound.h
+//Deskripsi: File ini berguna untuk memanggil fungsi dari file sound.c
+
+
+#ifndef SOUND_H
+#define SOUND_H
+
+#include <raylib.h>
+
+// 🔹 Inisialisasi & Pembersihan Efek Suara
+void InitSoundEffects();
+void UnloadSoundEffects();
+
+// 🔹 Efek Suara
+void PlayButtonClick();
+void PlayPaddleHit();
+void PlayBlockHit();
+void PlayLoseLife();
+void PlayGameOver();
+void PlayGameWin();
+
+// 🔹 Musik Latar
+void PlayBackgroundMusic();
+void StopBackgroundMusic();
+void PauseMusic();
+void ToggleMusic();
+void ChangeMusic(const char *filename);
+void UpdateMusic();
+
+// 🔹 Kontrol Volume
+void SetVolume(float volume);
+void IncreaseVolume();
+void DecreaseVolume();
+
+#endif
