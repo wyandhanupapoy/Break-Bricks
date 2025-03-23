31b3f3f32923936f6878ac235a0e2c6685907374
nsitizen <sitisoviyyah@gmail.com>
2025-03-23 22:30:13 +0700

    ambil game_state.h dari main

---
+++ game_state.h
@@ -0,0 +1,18 @@
+#ifndef GAME_STATE_H
+#define GAME_STATE_H
+
+typedef enum {
+    GAME_MENU,
+    GAME_START,
+    GAME_PLAY,
+    GAME_OVER,
+    GAME_WIN
+} GameState;
+
+// Screen size
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 650
+
+#define MAX_PLAYERS 1000
+
+#endif // GAME_STATE_H
