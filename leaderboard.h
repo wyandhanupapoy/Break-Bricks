48045c5e909b7dfe34c776b00a7450d335571aeb
wyandhanupapoy <wyandhanupapoy@gmail.com>
2025-03-23 21:34:30 +0700

    code fix untuk ledaerborad

--- leaderboard.h
+++ leaderboard.h
@@ -3,16 +3,35 @@
 
 #include <raylib.h>
 
-#define MAX_PLAYERS 10 // Maximum number of players in the leaderboard
+#define MAX_NAME_LENGTH 20
+#define MAX_LEADERBOARD_ENTRIES 1000
+#define LEADERBOARD_FILE "src/leaderboard.dat"
+#define SCROLL_SPEED 10
+#define SILVER (Color){192, 192, 192, 255}  // Define silver color
+#define BRONZE (Color){205, 127, 50, 255}   // Define bronze color
 
-typedef struct {
-    char name[20]; // Player's name
-    int score;     // Player's score
-    float time;    // Time taken to finish the game
+typedef struct
+{
+    char name[MAX_NAME_LENGTH];
+    int score;
+    float time;
+    int level;
+    char status[10]; // 🔹 Tambahkan status ("WIN" atau "GAME OVER")
 } LeaderboardEntry;
 
-void InitLeaderboard(LeaderboardEntry leaderboard[MAX_PLAYERS]);
-void AddToLeaderboard(LeaderboardEntry leaderboard[MAX_PLAYERS], const char* name, int score, float time);
-void DrawLeaderboard(LeaderboardEntry leaderboard[MAX_PLAYERS]);
+void InitLeaderboard(LeaderboardEntry leaderboard[MAX_LEADERBOARD_ENTRIES]);
+void AddToLeaderboard(LeaderboardEntry leaderboard[MAX_LEADERBOARD_ENTRIES],
+                      const char *name, int score, float time, int level, const char *status);
+void DrawLeaderboard(LeaderboardEntry leaderboard[MAX_LEADERBOARD_ENTRIES], int x, int y);
+void DrawLeaderboardMenu(LeaderboardEntry *leaderboard, int totalEntries, int scrollOffset);
+void SaveLeaderboard(LeaderboardEntry leaderboard[MAX_LEADERBOARD_ENTRIES]);
+void LoadLeaderboard(LeaderboardEntry leaderboard[MAX_LEADERBOARD_ENTRIES]);
+void SortLeaderboard(LeaderboardEntry leaderboard[MAX_LEADERBOARD_ENTRIES]);
 
-#endif // LEADERBOARD_H
+void LoadMedalTextures(void);
+void UnloadMedalTextures(void);
+
+void SetMedalScale(float scale);
+void SetMedalSize(int width, int height);
+
+#endif
